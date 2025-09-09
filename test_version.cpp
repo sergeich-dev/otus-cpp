8da2226167fe840550636da6238dbba52ecd7e4a
vitaly <sergeichm54@gmail.com>
2025-09-09 21:15:36 +0300

    work on homework

--- test_version.cpp
+++ test_version.cpp
@@ -2,12 +2,21 @@
 
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version) {
 	BOOST_CHECK(version() > 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_string_equality) {
+        std::string s1 = "hello";
+        std::string s2 = "hello";
+        std::string s3 = "world";
+
+        BOOST_TEST(s1 == s2);
+        BOOST_TEST(s1 != s3);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
