06044cafe38e9a8371b7f8f055d4e1f0ffeadc97
vitaly <sergeichm54@gmail.com>
2025-11-21 23:20:35 +0300

    Add sparse matrix

--- main.cpp
+++ main.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
-#include "editor/controller/controller.h"
-
-void onCreateDocument(CController * pController)
-{
-    pController->CreateDocument();
-}
-
-void onImportDocument(CController * pController, const std::string & path)
-{
-    pController->ImportDocument(path);
-}
-
-void onExportDocument(CController * pController, const std::string & path)
-{
-    pController->ExportDocument(path);
-}
-
-void onCreatePrimitive(CController * pController, ePrimitiveObjectType eObjType)
-{
-    pController->AddObject(eObjType);
-}
-void onRemovePrimitive(CController * pController, uint32_t nObjId)
-{
-    pController->RemoveObject(nObjId);
-}
+#include <cassert>
+#include "utils.h"
+#include "matrix.h"
 
 int main()
 {
     try
     {
-        CController controller;
+        Matrix<int, -1> matrix;
+
+        assert(matrix.size() == 0);
+
+        auto a = matrix[0][0];
+
+        assert(a == -1);
+        assert(matrix.size() == 0);
+
+        FillMatrixMainDiagonal(matrix, {0,0}, {9,9});
+
+        FillMatrixSecondaryDiagonal(matrix, {0,9}, {9,0});
+
+        std::cout << "Range from [0,0] to [8,8]: " << std::endl;
+
+        PrintMatrix(matrix, {0,0}, {8,8});
+
+        std::cout << std::endl;
+
+        std::cout << "Range from [1,1] to [8,8]: " << std::endl;
+
+        PrintMatrix(matrix, {1,1}, {8,8});
+
+        std::cout << std::endl;
+
+        // Prints total=17 because for main diagonal and secondary
+        // one element is common
+        std::cout << "Total filled cells in matrix: " << matrix.size() << " (for main diagonal and secondary one element is common)" << std::endl;
+
+        PrintMatrixCellsWithValues(matrix);
     }
     catch(const std::exception &e)
     {
@@ -38,3 +45,4 @@ int main()
 
     return 0;
 }
+
--- utils.h
+++ utils.h
@@ -69,4 +69,56 @@ void FillCustomContainer(T & custom)
     }
 }
 
+/**
+ * @brief  Fill matrix main diagonal with values [0,9]
+ */
+template<class T>
+void FillMatrixMainDiagonal(T & matrix, std::pair<int,int> startRange, std::pair<int,int> endRange)
+{
+    for (int i = startRange.first; i < endRange.first; ++i)
+    {
+        for (int j = startRange.second; j < endRange.second; ++j)
+            if (i == j)
+                matrix[i][j] = i;
+    }
+}
+
+/**
+ * @brief  Fill matrix secondary diagonal with values [9,0]
+ */
+template<class T>
+void FillMatrixSecondaryDiagonal(T & matrix, std::pair<int,int> startRange, std::pair<int,int> endRange)
+{
+    for (int i = startRange.first; i < endRange.first; ++i)
+    {
+        for (int j = startRange.second; j >= endRange.second; --j)
+            if (j == startRange.second - 1 - i)
+                matrix[i][j] = j;
+    }
+}
+
+template<class T>
+void PrintMatrix(const T & matrix, std::pair<int,int> startRange, std::pair<int,int> endRange)
+{
+    for (int i = startRange.first; i <= endRange.first; ++i)
+    {
+        for (int j = startRange.second; j <= endRange.second; ++j)
+            std::cout << matrix[i][j] << " ";
+
+        std::cout << std::endl;
+    }
+}
+
+
+template<class T>
+void PrintMatrixCellsWithValues(const T & matrix)
+{
+    for (const auto el : matrix)
+    {
+        std::cout << "[" << el.x << "][" << el.y << "]=" << el.v << ", ";
+    }
+
+    std::cout << std::endl;
+}
+
 #endif //_TOOLS_H_
